25c0f30050e92fe9d4d9cb6f2a26c54fbfd32813
Christos Paschalidis <paschalidi@ceid.upatras.gr>
2018-04-23 08:21:26 +0200

    master: Index

---
+++ src/components/Index/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AvatarPicker from '../AvatarPicker';
+
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <AvatarPicker />
+      </div>
+    );
+  }
+}
+
+export default Index;
